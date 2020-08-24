d3d7decf727304127516188ba61a0a38def334cd
Quentin Wilkerson Jr <quentin.a.wilkerson@gmail.com>
2020-08-24 03:21:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,44 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # top left -> bot right diag
+  [2,4,6]  # bot left -> top right diag
+]
+
+def matching_tokens?(set)
+  if (set.all? {|token| token == "X"} || set.all? {|token| token == "O"})
+    return true
+  else return false
+  end
+end
+
+def won?(board)
+  win = false
+  WIN_COMBINATIONS.each {|combination|
+    win_position1 = combination[0]
+    win_position2 = combination[1]
+    win_position3 = combination[2]
+    check = [board[win_position1], board[win_position2], board[win_position3]]
+      if (matching_tokens?(check))
+        win = combination
+      end
+  }
+  return win
+end
+
+def full?(board)
+  full = true
+  board.each{|index|
+    if !position_taken?(board, index)
+      full = false
+    end
+  }
+  return full
+end
